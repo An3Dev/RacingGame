75d0c04b36216605ba8a345d536276c4b4bd4c0a
Andres Nedilskyj <an3developer@gmail.com>
2021-04-01 16:22:57 -0700

    Initial commit

---
+++ Assets/Scripts/CarController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class CarController : MonoBehaviour
+{
+    [SerializeField] float forwardSpeed;
+    [SerializeField] AnimationCurve accelerationCurve;
+    [SerializeField] float brakeForce;
+
+    Rigidbody rb;
+
+
+    Vector3 worldVelocity = Vector3.zero;
+    Vector2 movementInput;
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    public void ReceiveMovementInput(Vector2 moveDir)
+    {
+        movementInput = moveDir;
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 velocity = new Vector3(movementInput.x, 0, movementInput.y) * forwardSpeed * Time.fixedDeltaTime;
+        Debug.Log(velocity.ToString("F3"));
+
+        velocity.y = rb.velocity.y;
+        rb.velocity = velocity;
+    } 
+}
---
+++ Assets/Scripts/CarInput.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(CarController))]
+public class CarInput : MonoBehaviour
+{
+    CarController carController;
+    private void Awake()
+    {
+        carController = GetComponent<CarController>();    
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        //Debug.Log(context.action.ToString() + ": " +context.ReadValue<Vector2>().ToString());
+        carController.ReceiveMovementInput(context.ReadValue<Vector2>());
+    }
+
+    public void OnLook(InputAction.CallbackContext context)
+    {
+        //Debug.Log(context.action.ToString() + ": " + context.ReadValue<Vector2>().ToString());
+    }
+}
